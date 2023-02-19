b0638bf8d03e9e539799fef670cc2c97dbff23bf
BigWolf21 <122544707+BigWolf21@users.noreply.github.com>
2023-02-19 23:58:21 +0700

    Создание HW01 решение первой задачи

---
+++ HW01/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задача 19
+Напишите программу, 
+которая принимает на вход пятизначное 
+число и проверяет, является ли оно 
+палиндромом. */
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int[] value = new int[5];
+int index;
+index = 0;
+while (index < 5)
+{
+  value[index] = new Random().Next(0, 10);
+  index++;
+}
+Console.WriteLine(value[0].ToString() + value[1] + value[2] + value[3] + value[4]);
+
+if (value[0] == value[4])
+{
+  if (value[1] == value[3])
+  {
+    Console.WriteLine("Число палиндром!");
+  }
+  else
+  {
+    Console.WriteLine("Число не палиндром!");
+  }
+}
+else
+{
+  Console.WriteLine("Число не палиндром!");
+}
